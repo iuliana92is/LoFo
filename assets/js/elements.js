977e9ab83c08cdaeac52129b07fc8fccc11ec2e1
iuciobanu <iuliana92is@yahoo.com>
2016-04-11 17:41:05 +0300

    remove js

---
+++ assets/js/elements.js
@@ -0,0 +1,26 @@
+
+// function addClass(el, className) {
+//     var el = document.querySelectorAll(el);
+    
+//     for (i = 0; i < el.length; i++) {
+//       if (el.classList) {
+//         el[i].classList.add(className);
+//       } else {
+//         el[i].className += ' ' + className;
+//       }
+//     }
+// }
+
+
+// function removeClass(el, className) {
+//     var el = document.querySelectorAll(el);
+    
+//     for (i = 0; i < el.length; i++) {
+//       if (el[i].classList) {
+//         el[i].classList.remove(className);
+//       } else {
+//         el[i].className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+//       }
+//     }
+// }
+ 
