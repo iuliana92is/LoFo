e60703dbe58db9c65144954303fee6cde6dc23df
iuciobanu <iuliana92is@yahoo.com>
2016-05-06 16:05:26 +0300

    userpage

---
+++ user.php
@@ -0,0 +1,44 @@
+<article class="user">
+	<h1>Nume User:</h1>
+
+	<div class="informatiileMele">
+		<p>Informatiile Mele:</p>
+	</div> 
+	<br /> 
+	<section id="infoMele"> 
+		<ul class="infoMele">
+			<li>Nume:</li>
+			<li>Adesa e-mail:</li>
+			<li>Telefon:</li> 
+		</ul>
+	</section>	
+	<br /> 
+	<br /> 
+	<br /> 
+	<div class="listaPostari">
+		<p>Lista anunturilor postate de mine:</p>
+	</div>
+	<br />
+	<section id="anunturiUtilizator"> 
+        <ul class="infoAnunt">
+            <li class="anuntulMeu"> nume anunt 1</li>
+            <li class="editareAnunt">editare</li>
+            <li class="eliminareAnunt">eliminare</li> 
+            <li class="vizualizareAnunt">vizualizare</li>
+        </ul> 
+         <ul class="infoAnunt">
+           	<li class="anuntulMeu">nume anunt 2</li>
+            <li class="editareAnunt">editare</li>
+            <li class="eliminareAnunt">eliminare</li> 
+            <li class="vizualizareAnunt">vizualizare</li>
+        </ul> 
+         <ul class="infoAnunt">
+            <li class="anuntulMeu">nume anunt 3</li>
+            <li class="editareAnunt">editare</li>
+            <li class="eliminareAnunt">eliminare</li> 
+            <li class="vizualizareAnunt">vizualizare</li>
+        </ul>
+	</section> 
+	<div class="clearfix"></div>
+</article>
+<div class="clearfix"></div>
