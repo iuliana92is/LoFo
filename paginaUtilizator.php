8dfc86498827728be18714810c5798569d187fcc
iuciobanu <iuliana92is@yahoo.com>
2016-05-26 10:24:43 +0300

    refactored modal design edit announcement

--- paginaUtilizator.php
+++ paginaUtilizator.php
@@ -7,6 +7,46 @@
     }
 ?> 
 
+<div id="modalAnunt"   >
+    <div id="inchideModal"onclick="inchidereModal()">
+        <img src="assets/images/icons/close.png">
+    </div>
+    <h1>Nume anunt</h1>
+    <form class="dateModalAnunt">
+        <select class="categorie">
+            <option disabled="" selected="">Categorie</option>
+            <option>Animale</option>
+            <option>Obiecte</option>
+        </select> 
+        <select class="zona">
+            <option disabled="" selected="">Zona</option>
+            <option>Pacurari</option>
+            <option>Dacia</option> 
+            <option>Valea lupului</option>
+        </select>
+        <input type="text" placeholder="nume"/>
+         <select class="culoare">
+            <option disabled="" selected="">Culoare</option>
+            <option> Rosu</option>
+            <option> Galben</option> 
+        </select>
+           <select class="stare">
+            <option disabled="" selected="">Stare</option>
+            <option>Buna</option>
+            <option>Deteriorat</option>
+        </select>
+        <div class="upload">
+            <input type="file" name="upload"/> 
+        </div>
+    </form>
+    <br/>
+    </br>
+    <div id="butoaneModal">
+        <button name="salveazaEditare" value="Send" class="salveazaEditare"  >Salveaza editarea</button>
+        <button name="anuleazaEditare" value="Cancel" class="anuleazaEditare" >Reseteaza campuri</button>
+    </div>
+</div>
+
 
 <article class="user">
     <h1>Nume User:</h1>
--- user.php
+++ user.php
@@ -1,3 +1,42 @@
+<div id="modalAnunt"  class="hidden" >
+    <div id="inchideModal"onclick="inchidereModal()">
+        <img src="assets/images/icons/close.png">
+    </div>
+    <h1>Nume anunt</h1>
+    <form class="dateModalAnunt">
+        <select class="categorie">
+            <option disabled="" selected="">Categorie</option>
+            <option>Animale</option>
+            <option>Obiecte</option>
+        </select> 
+        <select class="zona">
+            <option disabled="" selected="">Zona</option>
+            <option>Pacurari</option>
+            <option>Dacia</option> 
+            <option>Valea lupului</option>
+        </select>
+        <input type="text" placeholder="nume"/>
+         <select class="culoare">
+            <option disabled="" selected="">Culoare</option>
+            <option> Rosu</option>
+            <option> Galben</option> 
+        </select>
+           <select class="stare">
+            <option disabled="" selected="">Stare</option>
+            <option>Buna</option>
+            <option>Deteriorat</option>
+        </select>
+        <div class="upload">
+            <input type="file" name="upload"/> 
+        </div>
+    </form>
+    <br/>
+    </br>
+    <button name="salveazaEditare" value="Send" class="salveazaEditare"  >Salveaza editarea</button>
+    <button name="anuleazaEditare" value="Cancel" class="anuleazaEditare" >Reseteaza campuri</button>
+</div>
+
+
 <article class="user">
     <h1>Nume User:</h1>
 
